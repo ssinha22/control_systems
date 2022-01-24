12b55d7ea273a778ef5e7d349ddd52681798ea63
ssinha22 <55689259+ssinha22@users.noreply.github.com>
2022-01-24 10:48:02 +0700

    Update bno055_reader.py

--- bno055_reader.py
+++ bno055_reader.py
@@ -1,1 +1,54 @@
 
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_bno055
+
+
+i2c = board.I2C()
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+
+# If you are going to use UART uncomment these lines
+# uart = board.UART()
+# sensor = adafruit_bno055.BNO055_UART(uart)
+
+last_val = 0xFFFF
+
+
+def temperature():
+    global last_val  # pylint: disable=global-statement
+    result = sensor.temperature
+    if abs(result - last_val) == 128:
+        result = sensor.temperature
+        if abs(result - last_val) == 128:
+            return 0b00111111 & result
+    last_val = result
+    return result
+
+
+while True:
+    print("Temperature: {} degrees C".format(sensor.temperature))
+    #Measures the temperature of the chip in degrees Celsius.
+    """
+    
+    print(
+        "Temperature: {} degrees C".format(temperature())
+    )  # Uncomment if using a Raspberry Pi
+    """
+    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
+    #Gives the raw accelerometer readings, in m/s. Returns an empty tuple of length 3 when this property has been disabled by the current mode
+    
+    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
+    #Gives the raw magnetometer readings in microteslas. Returns an empty tuple of length 3 when this property has been disabled by the current mode.
+    
+    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
+    print("Euler angle: {}".format(sensor.euler))
+    print("Quaternion: {}".format(sensor.quaternion))
+    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
+    print("Gravity (m/s^2): {}".format(sensor.gravity))
+    #Returns the gravity vector, without acceleration in m/s. Returns an empty tuple of length 3 when this property has been disabled by the current mode.
+    print()
+
+    time.sleep(1)
