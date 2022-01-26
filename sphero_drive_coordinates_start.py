8e737653d8b3b011856ec4605112512e0d9fe5d0
ssinha22 <55689259+ssinha22@users.noreply.github.com>
2022-01-26 10:46:03 +0700

    Update sphero_drive_coordinates_start.py

--- sphero_drive_coordinates_start.py
+++ sphero_drive_coordinates_start.py
@@ -28,8 +28,8 @@ PI = 3.14159
 # and then rotate to an angle of PI_OVER_3 (60 degrees).
 
 # Write your code here to get the RVR to move in an equilateral triangle of 1 meters per side.
- 
-
+rvr.drive_to_position_si(PI_OVER_3,0,0,SPEED)
+time.sleep(3.0)
 rvr.drive_to_position_si(PI_OVER_3,0.5,1.732/2,SPEED)
 time.sleep(3.0)
 rvr.drive_to_position_si(PI_OVER_3,1,0,SPEED)
