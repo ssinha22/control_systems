2a80bdd2d9cc4a770948f7ffbb7b5d89199d6de1
ssinha22 <55689259+ssinha22@users.noreply.github.com>
2022-02-11 10:33:39 +0700

    Create cp_task_4.py

---
+++ cp_light_control/cp_task_4.py
@@ -0,0 +1,12 @@
+# Write your code here :-)
+import time
+import board
+from analogio import AnalogOut, AnalogIn
+
+analog_out = AnalogOut(board.A0)
+analog_in = AnalogIn(board.A2)
+
+while True:
+    # Count up from 0 to 65535, with 64 increment
+    # which ends up corresponding to the DAC's 10-bit range
+    analog_out.value = analog_in.value
