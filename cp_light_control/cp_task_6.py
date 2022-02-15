c16954e97db7dbd8666fca966bd2c04d0a3b12f9
ssinha22 <55689259+ssinha22@users.noreply.github.com>
2022-02-15 10:41:57 +0700

    Update cp_task_6.py

--- cp_light_control/cp_task_6.py
+++ cp_light_control/cp_task_6.py
@@ -1,4 +1,5 @@
 # Write your code here :-)
+# Write your code here :-)
 import time
 import board
 from analogio import AnalogOut, AnalogIn
@@ -7,13 +8,14 @@ analog_out = AnalogOut(board.A0)
 analog_in = AnalogIn(board.A2)
 
 k = 1
-
-
+ktwo = 0.1
+sum_error = 0
 
 while True:
     # Count up from 0 to 65535, with 64 increment
     # which ends up corresponding to the DAC's 10-bit range\
     error = 60000 - analog_in.value
-    adjust = error*k
+    sum_error += error
+    adjust = error*k - sum_error*ktwo
     analog_out.value += adjust
     print(analog_in.value)
